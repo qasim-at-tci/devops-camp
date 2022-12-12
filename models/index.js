c4e07f5fc109b73698f889be4e1991f96cdf3ba0
Qasim Abdullah <qasim.abdullah@travis-ci.org>
2022-12-12 13:00:44 +0500

    add env vars

--- models/index.js
+++ models/index.js
@@ -1,6 +1,8 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize("postgres://postgres:password@db:5432/tasks");
+const sequelize = new Sequelize(
+  `postgres://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:5432/tasks`
+);
 
 const db = {};
 
