e4ca45f0fe18521cfd5440b434ec69f968ed79af
Qasim Abdullah <qasim.abdullah@travis-ci.org>
2022-12-14 02:14:14 +0500

    .

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const db = require("./models");
 db.sequelize
   .sync()
   .then(() => {
-    console.log("Synced db!");
+    console.log("Synced db.");
   })
   .catch((err) => {
     console.log("Failed to sync db: " + err.message);
